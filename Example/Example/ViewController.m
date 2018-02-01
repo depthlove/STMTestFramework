ee3d9adaed0aa0031b35a6eb2af8f14a3e34972c
depthlove <suntongmian@163.com>
2018-02-01 12:02:44 +0800

    Add .podspec

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <STMTestFramework/STMTestFramework.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    STMTestFrameworkClass *stmTestFrameworkClass = [[STMTestFrameworkClass alloc] init];
+    [stmTestFrameworkClass ouputFrameworkName];
 }
 
 
